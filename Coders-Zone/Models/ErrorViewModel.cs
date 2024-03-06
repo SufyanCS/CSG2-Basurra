307b7f657df21e31e65c8f966b473c506e7fe6af
Sufyan <Sufyan@DESKTOP-M5GGP41>
2024-03-06 19:46:25 +0300

    Add project files.

---
+++ Coders-Zone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coders_Zone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
