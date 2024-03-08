fb2d309f82f5ca00a33149868eca7c965a47ad8f
Sufyan <Sufyan@DESKTOP-M5GGP41>
2024-03-08 23:40:34 +0300

    Create empty index files and controllers for upcoming pages

---
+++ Coders-Zone/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coders_Zone.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
