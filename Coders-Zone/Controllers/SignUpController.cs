57d5f6943e3c2b43bd85c712625205d9fe300e6e
sheem <alshym.x0@gmail.com>
2024-04-13 00:24:21 +0300

    Linking account creation and login pages to the database with data validation procedures

--- Coders-Zone/Controllers/SignUpController.cs
+++ Coders-Zone/Controllers/SignUpController.cs
@@ -1,12 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using Coders_Zone.Data;
+using Coders_Zone.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 namespace Coders_Zone.Controllers
 {
+
+    
     public class SignUpController : Controller
     {
+        private readonly CoderZoneDbContext _Db;
+
+        public SignUpController(CoderZoneDbContext db)
+        {
+            _Db = db;   
+        }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(User signUp)
+        {
+           
+            string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+
+
+            //if (!ModelState.IsValid)
+            //{
+
+            //    return View(signUp);
+
+
+            //}
+            if (string.IsNullOrEmpty(signUp.Username) || string.IsNullOrEmpty(signUp.Email) || string.IsNullOrEmpty(signUp.HashedPassword))
+            {
+               // ModelState.AddModelError("", "Please fill in all the fields");
+                return View(signUp);
+            }
+
+            if (!Regex.IsMatch(signUp.Email, pattern))
+                {
+                    ModelState.AddModelError("Email", "Incorrect e-mail format");
+                    return View(signUp);
+
+                }
+                if (IsValidateUser(signUp.Username))
+                {
+
+                    ModelState.AddModelError("Username", "UserName was used before,Please enter another Username");
+                    return View(signUp);
+
+                }
+           
+                
+                else
+                {
+                _Db.Users.Add(signUp);
+                _Db.SaveChanges();
+                return RedirectToAction("index", "Courses");
+                 }
+
+
+
+        }
+        public bool IsValidateUser(string username)
+        {
+            var user = _Db.Users.FirstOrDefault(u => u.Username==username);
+
+            return user != null;
         }
     }
 }
--- Coders-Zone/Models/User.cs
+++ Coders-Zone/Models/User.cs
@@ -1,17 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Coders_Zone.Models
 {
     public class User
     {
         //public string UserName { get; internal set; }
-        public int Id { get; set; } // Auto-incrementing ID
+        [Key]
+        public int Id { get; set; }
+        // Auto-incrementing ID
+        [Required]
+        [DisplayName("User Name")]
+      
         public string Username { get; set; }
+        [Required]
+        
         public string Email { get; set; }
+        [Required]
+        [DisplayName("Password")]
         public string HashedPassword { get; set; } // Hashed password
-        public DateTime RegistrationDate { get; set; }
-        public bool IsAdmin { get; set; }
-        public bool IsBanned { get; set; } // New property to indicate whether the user is banned or not
+        public DateTime RegistrationDate { get; set; }= DateTime.Now;
+        public bool IsAdmin { get; set; }= false;
+        public bool IsBanned { get; set; } = false; // New property to indicate whether the user is banned or not
         public ICollection<UserCourse> UserCourses { get; set; } // Navigation property for UserCourses
         public ICollection<Review> Reviews { get; set; } // Navigation property for Reviews
         public ICollection<UserLesson> UserLessons { get; set; }
